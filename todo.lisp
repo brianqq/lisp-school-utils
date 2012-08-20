84fdba5c0e60c5fa385ffeb88aa17e736d388cca
Brian Levy <brianlevy95@gmail.com>
2012-08-19 23:07:27 -0400

    Added some stuff

--- todo.lisp
+++ todo.lisp
@@ -1,11 +1,11 @@
 (ql:quickload 'cl-ppcre)
 (ql:quickload 'trivial-timers)
-
 (defpackage #:todo
-  (:use :cl :cl-ppcre :trivial-timers)
+  (:use :cl :cl-ppcre :trivial-timers :qt)
   (:export :*todo-location*
 	   :parse-org-text
-	   :get-today))
+	   :get-today
+	   :make-timers))
 
 (in-package :todo)
 
@@ -17,7 +17,7 @@
     (apply function (append args more-args))))
 
 ;;; File processing
-(defvar *todo-location*)
+(defvar *todo-location* "/home/brian/Documents/todo/")
 
 (defun today-string ()
   "Returns the date as \"ddmmyyyy\""
@@ -26,43 +26,46 @@
     (declare (ignorable sec min hour))
     (format nil "~2,'0d~2,'0d~d" date month year)))
 
-(defun get-today ()
-  "Opens today's todo file in *todo-location*/ddmmyyyy.org"
-  (with-open-file (todo (make-pathname :directory *todo-location*
-				       :name (today-string)
-				       :type "org"))
-    (slurp-stream todo)))
-
 ;;;see http://www.ymeme.com/slurping-a-file-common-lisp-83.html
 (defun slurp-stream (stream)
   (let ((seq (make-string (file-length stream))))
     (read-sequence seq stream)
     seq))
 
+(defun get-today ()
+  "Opens today's todo file in *todo-location*/ddmmyyyy.org"
+  (with-open-file (todo (make-pathname :directory *todo-location*
+				       :name (today-string)
+				       :type "org")
+			:direction :inpu)
+    (slurp-stream todo)))
+
 (defun interp-time (string)
   (multiple-value-bind (_ time) (scan-to-strings "(\\d{1,2}):(\\d\\d)" string)
+    (declare (ignorable _))
     (map 'list #'parse-integer time)))
 
 (defun parse-org-text (text)
  (let ((acc))
-   (do-scans (start
-	      end reg-start reg-end
+   (do-scans (start end reg-start reg-end
 	      "\\*\\sTODO\\s(.+?)\\n\\s+?(?!CLOSED)\\s+?SCHEDULED:\\s+?<(.+)>"
 	      text (nreverse acc))
      (push 
       (list (subseq text (aref reg-start 0) (aref reg-end 0))
-	    (interp-time  (subseq text (aref reg-start 1)
-				  (aref reg-end 1))))
+	    (interp-time (subseq text (aref reg-start 1) (aref reg-end 1))))
       acc))))
 
-;;;File processing ends her
+;;;File processing ends here
 
-(defun alert (x) (format t x))
+(defun alert (x) (format t "~a~%" x))
 
 (defun make-timers (action-list)
   (loop for (text (hour min)) in action-list do
        (schedule-timer (make-timer (curry #'alert text))
 		       (multiple-value-bind (_s _m _h date month year)
 			   (get-decoded-time)
+			 (declare (ignorable _s _m _h))
 			 (encode-universal-time 0 min hour date month year))
 		       :absolute-p t)))
+
+(make-timers (parse-org-text (get-today)))
